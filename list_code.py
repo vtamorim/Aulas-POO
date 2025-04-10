46665906c9d9ec5f6f1cfd0297622d8fd9216448
Victor Miguel <vthero89@gmail.com>
2025-04-10 12:26:09 +0000

    Guia de Uso de Codespace

---
+++ list_code.py
@@ -0,0 +1,7 @@
+#Exercício 1
+print("Nome: Victor Miguel Amorim do Nascimento")
+print("Matrícula: 20241011110003")
+#Exercício 2 
+nome = input("Digite seu primeiro nome: \n")
+
+print(f"Bem-vindo ao Codespace: {nome}")
