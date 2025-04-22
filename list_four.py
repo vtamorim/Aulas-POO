ba381e9ab304806340275185e2a237c78108e2b4
Victor Miguel <vthero89@gmail.com>
2025-04-22 11:54:51 -0300

    Terminei one

--- list_four.py
+++ list_four.py
@@ -4,13 +4,14 @@ match casos:
     case 1:
         numeros = list(map(float, input().split()))
         delta = (numeros[1]**2) - (4 * numeros[0] * numeros[2])
-        if delta < 0:
-            print("Delta Negativo")
 
-        if numeros[0] <= 0:
-            print("Valor de A Inválido")
+        if numeros[0] == 0 or delta<0:
+            print("Impossivel calcular")
         else:
             x1 = (-numeros[1] + math.sqrt(delta)) / (2 * numeros[0])
             x2 = (-numeros[1] - math.sqrt(delta)) / (2 * numeros[0])
-            print(f"Raízes: {x1}, {x2}")
+            
+            print(f"R1 = {x1:.5f}")
+            print(f"R2 = {x2:.5f}")
+
 
